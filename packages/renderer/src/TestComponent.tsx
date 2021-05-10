3678ba7af1b52ed552f71347720ae321cf234379
Peter Todd <ptr.tdd@gmail.com>
2021-05-10 11:46:54 +0100

    testing file structure

--- packages/renderer/src/TestComponent.tsx
+++ packages/renderer/src/TestComponent.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import meaning from '../../common/model';
 
 export default function test() {
   return (
-    <h1>All is well.</h1>
-  )
+    <h1>All is well. {meaning}</h1>
+  );
 }
